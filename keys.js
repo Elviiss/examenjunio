c79a9d05809a007ad4206cbc735eb955e4fd541e
Elviiss <elvis22elmejor@gmail.com>
2023-06-15 13:45:28 +0200

    añadiendo el clever

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: "localhost",
-        user: "root",
-        password: "root",
-        database: "exajunio"
+        host: "blh6v2r6gpkyta1jfa6a-mysql.services.clever-cloud.com",
+        user: "u857enpsnmyh83no",
+        password: "7DbGwc0adPvpUwb4mLet",
+        database: "blh6v2r6gpkyta1jfa6a"
     }
 }
